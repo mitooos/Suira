05e1678039e91aeb149a7ddfcdb1542ffb1adf58
mitooos <ma.hoyosr@uniandes.edu.co>
2020-01-24 14:19:11 -0500

    allowed hosts

--- backOffice/settings.py
+++ backOffice/settings.py
@@ -34,7 +34,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['3.133.157.86',]
 
 
 # Application definition
