8f807171314c8b51e6380e8149057a8f39b32222
coldblade2000 <diegogranada7@gmail.com>
2020-01-23 20:20:51 -0500

    nombres arreglados

--- cliente/models.py
+++ cliente/models.py
@@ -4,6 +4,6 @@ from django.db import models
 class Cliente(models.Model):
     nombre = models.CharField(max_length=50)
     email = models.EmailField(max_length=50)
-    nTrabajos = models.IntegerField()
-    proximaFecha = models.DateField(blank=True)
+    n_trabajos = models.IntegerField()
+    proxima_fecha = models.DateField(blank=True)
     comentarios = models.TextField(blank=True, null=True)
