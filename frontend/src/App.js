4114fd6585fd87f2db83d0d1d5b7ee3efd02df56
Miguel Hoyos ruge <ma.hoyosr@uniandes.edu.co>
2020-01-29 09:20:12 -0500

    se agregan las rutas al router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,38 +1,48 @@
 import React from 'react';
-import logo from './logo.svg';
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.css';
 
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+import ClienteList from './components/cliente/cliente-list.component';
+import ClienteCreate from './components/cliente/cliente-create.component';
+import ClienteDetail from './components/cliente/cliente-detail.component';
+import ClienteUpdate from './components/cliente/cliente-update.component';
+
 function App() {
-  return (
+  return (<Router>
     <div className="App">
       <Navbar bg="light" expand="sm">
         <Navbar.Brand href="/">Suira</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="mr-auto">
-            <Nav.Link href="/">Clientes</Nav.Link>
+            <Nav.Link href="/clientes" >Clientes</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Navbar>
 
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Container>
+        <Row>
+          <Col>
+            <div className="wrapper">
+              <Switch>
+                <Route exact path="/clientes" component={ClienteList}/>
+                <Route exact path="/clientes/create" component={ClienteCreate}/>
+                <Route exact path="/clientes/:id/update" component={ClienteUpdate}/>
+                <Route path="/clientes/:id" component={ClienteDetail}/>
+              </Switch>
+            </div>
+          </Col>
+        </Row>
+      </Container>
     </div>
+    </Router>
   );
 }
 
