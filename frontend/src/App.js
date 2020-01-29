62cee76cbfbe95f832a96dd3510887bf81a1ce68
Miguel Hoyos ruge <ma.hoyosr@uniandes.edu.co>
2020-01-29 09:02:48 -0500

    se agrega el navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,23 @@
 import React from 'react';
 import logo from './logo.svg';
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.css';
 
 function App() {
   return (
     <div className="App">
+      <Navbar bg="light" expand="sm">
+        <Navbar.Brand href="/">Suira</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="mr-auto">
+            <Nav.Link href="/">Clientes</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
