095bb14988978f9d00c62606384df09439d1fa88
Miguel Hoyos ruge <ma.hoyosr@uniandes.edu.co>
2020-02-03 19:04:22 -0500

    boton navegacion y prubas en la bd

--- frontend/src/components/cliente/cliente-detail.component.js
+++ frontend/src/components/cliente/cliente-detail.component.js
@@ -1,19 +1,22 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import axios from 'axios';
+import Button from 'react-bootstrap/Button';
+import {Link} from 'react-router-dom';
 
-export default class ClienteDetail extends Component{
+export default class ClienteDetail extends Component {
 
-    constructor(props){
+    constructor(props) {
         super(props);
         this.state = {
             cliente: ''
         };
     }
 
-    componentDidMount(){
+    componentDidMount() {
         axios.get(process.env.REACT_APP_API_URL + 'clientes/' + this.props.match.params.id + '/')
-            .then( res => {
+            .then(res => {
                 this.setState({
+                    id: res.data.id,
                     nombre: res.data.nombre,
                     email: res.data.email,
                     telefono: res.data.perfil.telefono,
@@ -23,14 +26,15 @@ export default class ClienteDetail extends Component{
                     comentarios: res.data.comentarios
                 });
             })
-            .catch( (error => {
+            .catch((error => {
                 console.log(error)
             }));
     }
 
-    render(){
-        return(
-            <ul>
+    render() {
+        return (
+           <div>
+                <ul>
                 <li>Nombre: {this.state.nombre}</li>
                 <li>Email: {this.state.email}</li>
                 <li>Telefono: {this.state.telefono} </li>
@@ -39,6 +43,10 @@ export default class ClienteDetail extends Component{
                 <li>Descripción: {this.state.descripcion}</li>
                 <li>Comentarios: {this.state.comentarios}</li>
             </ul>
+            <Link to={"/clientes/" + this.state.id+ '/update/'}>
+                <Button variant="primary" size="lg" block="block">Actualizar</Button>
+            </Link>
+           </div>
         );
     }
 }
